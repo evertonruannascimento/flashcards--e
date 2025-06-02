6c7d5ad1897cba1075a22ad879301b746a614ef5
evertonruannascimento <everton.ruan.nascimento@escola.pr.gov.br>
2025-06-02 14:02:54 +0000

    ok

---
+++ app.js
@@ -0,0 +1,5 @@
+funtion criaCartao(categoria,pergunta,respota){ 
+    let container = document.getElementById ('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+}
